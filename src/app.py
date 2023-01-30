45cbd773731a93ba6c7783918a7a21e96eebb286
YoungJae Kim <byeongal@comcom.ai>
2023-01-30 10:50:45 +0900

    remove comment

--- src/app.py
+++ src/app.py
@@ -9,7 +9,6 @@ from settings import server_settings
 def get_app() -> FastAPI:
     fast_api_app = FastAPI(title=server_settings.app_name, version=server_settings.app_version)
     fast_api_app.add_event_handler("startup", start_app_handler(fast_api_app))
-    # fast_api_app.add_event_handler("shutdown", stop_app_handler(fast_api_app))
     fast_api_app.include_router(router)
     return fast_api_app
 
